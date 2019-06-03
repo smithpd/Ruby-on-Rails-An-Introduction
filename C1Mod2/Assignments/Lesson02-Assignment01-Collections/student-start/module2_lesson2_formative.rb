0984e79e91c3ec2da8466f5a54d30ab77fdcc3b9
Tricia Smith <smithpd3@gmail.com>
2019-06-02 19:17:45 -0700

    Completed Lesson 2 Assignment 1

--- C1Mod2/Assignments/Lesson02-Assignment01-Collections/student-start/module2_lesson2_formative.rb
+++ C1Mod2/Assignments/Lesson02-Assignment01-Collections/student-start/module2_lesson2_formative.rb
@@ -4,9 +4,15 @@ p arr
 
 # This selects only elements that when divided by 3 have a remainder of 0 
 # using the % (modulus) operator
-p arr.select { |element| element % 3 == 0 }
+arr = arr.select { |element| element % 3 == 0 }
+p arr
 
 # Using `reject` method filter out anything less than 5000
+arr = arr.reject { |element| element < 5000 }
+p arr
+
 # and use `sort` and `reverse` methods to sort in descending order
+p arr.sort!.reverse!
+
 # Start with the line below and continue as 1 long method chain
 # p arr.select { |element| element % 3 == 0 }
